cad2a9b583423cb2de6b6c3eba25c907b684094b
Ashish-7218 <102752448+Ashish-7218@users.noreply.github.com>
2022-07-01 11:01:47 +0530

    Add files via upload

---
+++ CreateTable.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateTable {
+
+	public static void main(String[] args) {
+		
+		try {
+				Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/MyAssignment1", "root","Ashish@7218");
+				
+				Statement stmt = conn.createStatement();
+				
+			
+					String sql = "CREATE TABLE Employee" +
+			                "(EmpId INTEGER not NULL, " +
+			                " EmpName VARCHAR(255), " + 
+			                " Designation VARCHAR(255), " + 
+			                " age INTEGER, " + 
+			                " PRIMARY KEY ( EmpId ))"; 
+					
+					
+					stmt.executeUpdate(sql);
+					
+					System.out.println("Table Created in MyAssignment1");
+					
+					conn.close();
+					stmt.close();
+			
+			} catch (SQLException e){
+				
+				e.printStackTrace();
+		}				
+	}
+}
